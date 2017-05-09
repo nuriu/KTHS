6ef748a42f304ba223e1c37e81a5b0a48754cb19
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2017-05-09 14:15:38 +0300

    Harita stili ayarlandı.

--- app/src/main/java/com/nuriuzunoglu/kths/MapsActivity.java
+++ app/src/main/java/com/nuriuzunoglu/kths/MapsActivity.java
@@ -1,9 +1,12 @@
 package com.nuriuzunoglu.kths;
 
+import android.content.res.Resources;
 import android.location.Address;
 import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -12,13 +15,15 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+    private static final String TAG = MapsActivity.class.getSimpleName();
     private GoogleMap mMap;
     private EditText mLocationSearch;
 
@@ -42,6 +47,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mLocationSearch = (EditText) findViewById(R.id.map_search);
         mMap = googleMap;
 
+        // Harita stilini değiştir
+        try {
+            boolean success = googleMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style));
+
+            if (!success) {
+                Log.e(TAG, "Harita stili okunamadı.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Harita stili bulunamadı. Hata: ", e);
+        }
+
         // Sydney üzerine bir işaretçi koy ve kamerayı taşı
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Sydney üzerindeki işaretçi"));
@@ -49,6 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     public void onMapSearch(View view) {
+        mLocationSearch.clearFocus();
         String location = mLocationSearch.getText().toString();
         List<Address> addressList = null;
 
@@ -65,7 +83,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
 
             // Kamerayı ilk bulunan yere taşı
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
         }
 
         System.out.println(view);
